2b4c5f75e52795afc008fb18186d053c369d3aaf
NaviGH <naveensoundar95@gmail.com>
2022-12-23 16:29:30 +0530

    Added by American Arch

---
+++ src/newbbchange.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class newbbchange {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "D:\\Software\\BrowserDriver\\chromedriver.exe");
+
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://google.com");
+		System.out.println("new line 1");
+		System.out.println("new line 2");
+		//add
+		//delete
+		
+				
+	}
+
+}
